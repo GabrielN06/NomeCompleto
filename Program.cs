1be7e997759acd98c13235b431e117e3cd1a30e0
Gabriel Nunes <gabrielnunesdacostaanselmo@gmail.com>
2025-03-17 09:14:35 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string nome;
+string sobrenome;
+
+Console.Write("Digite seu primeiro nome: ");
+nome = Console.ReadLine()!;
+
+Console.Write("Digite seu sobrenome: ");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"Nome completo: {nome} {sobrenome}");
+Console.WriteLine($"Nome completo: {sobrenome.ToUpper()} {nome}");
